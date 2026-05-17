d68a2f66b5a8ada19eb94e25c9ed003c8a29fd0d
BrundevCoder <brunodevdrums@gmail.com>
2026-05-17 23:32:31 +0100

    JS functions working

--- script.js
+++ script.js
@@ -6,6 +6,33 @@ const fontNames = [
   "sans-serif", "serif", "Arial",
   '"Syne"', "monospace", "Verdana",
   '"Outfit"', "system-ui", "Impact",
-  "cursive", "Cambria", '"Courier New"'
-]
+  "cursive", "Cambria", '"Courier New"',
+  '""Trebuchet MS""', "Helvetica", '"Lucida Console"',
+  "Fantasy", "Georgia"
+];
+const fontColors = [
+  "#000", "#333"
+];
 
+function randomFontConfig() {
+  let data = {"Name": "Syne", "Weight": 100, "Color": "#000"};
+
+  data["Name"] = fontNames[Math.floor(Math.random() * fontNames.length)];
+  data["Weight"] = fontWeights[Math.floor(Math.random() * fontWeights.length)];
+  data["Color"] = fontColors[Math.floor(Math.random() * fontColors.length)];
+
+  return data;
+}
+
+function update() {
+  const fontData = randomFontConfig();
+
+  display.style.fontFamily = fontData["Name"];
+  display.style.fontWeight = fontData["Weight"];
+  display.style.color = fontData["Color"];
+
+}
+
+textInput.addEventListener("input", () => {
+  update()
+});
