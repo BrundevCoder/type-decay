dd397107cc7f4b17d0478dee693f7d8d7d028331
BrundevCoder <brunodevdrums@gmail.com>
2026-05-17 23:19:25 +0100

    started project

---
+++ script.js
@@ -0,0 +1,11 @@
+const display = document.getElementById("textDisplay");
+const textInput = document.getElementById("ipt");
+
+const fontWeights = [100, 200, 300, 400, 500, 600, 700, 800, 900];
+const fontNames = [
+  "sans-serif", "serif", "Arial",
+  '"Syne"', "monospace", "Verdana",
+  '"Outfit"', "system-ui", "Impact",
+  "cursive", "Cambria", '"Courier New"'
+]
+
